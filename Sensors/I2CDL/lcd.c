45d42ecee881f6632e04b1faac243816fa2cad11
Carlos Rodriguez <brahamcoso@gmail.com>
2016-07-15 23:31:46 +0000

    Making minor corrections
    This change makes more legible the lcd file
    
    Signed-off-by: Carlos Rodriguez <brahamcoso@gmail.com>

--- Sensors/I2CDL/lcd.c
+++ Sensors/I2CDL/lcd.c
@@ -25,37 +25,37 @@
 #define LCD_CLEARDISPLAY    0x01
 #define LCD_RETURNHOME      0x02
 #define LCD_ENTRYMODESET    0x04
-#define LCD_DISPLAYCONTROL    0x08
-#define LCD_CURSORSHIFT    0x10
-#define LCD_FUNCTIONSET 0x20
+#define LCD_DISPLAYCONTROL  0x08
+#define LCD_CURSORSHIFT     0x10
+#define LCD_FUNCTIONSET     0x20
 #define LCD_SETCGRAMADDR    0x40
 #define LCD_SETDDRAMADDR    0x80
 
 // flags for display entry mode
-#define LCD_ENTRYRIGHT    0x00
-#define LCD_ENTRYLEFT    0x02
+#define LCD_ENTRYRIGHT          0x00
+#define LCD_ENTRYLEFT           0x02
 #define LCD_ENTRYSHIFTINCREMENT 0x01
 #define LCD_ENTRYSHIFTDECREMENT 0x00
 
 // flags for display on/off control
-#define LCD_DISPLAYON    0x04
-#define LCD_DISPLAYOFF    0x00
+#define LCD_DISPLAYON   0x04
+#define LCD_DISPLAYOFF  0x00
 #define LCD_CURSORON    0x02
-#define LCD_CURSOROFF    0x00
-#define LCD_BLINKON    0x01
+#define LCD_CURSOROFF   0x00
+#define LCD_BLINKON     0x01
 #define LCD_BLINKOFF    0x00
 
 // flags for display/cursor shift
-#define LCD_DISPLAYMOVE 0x08
-#define LCD_CURSORMOVE    0x00
+#define LCD_DISPLAYMOVE  0x08
+#define LCD_CURSORMOVE   0x00
 #define LCD_MOVERIGHT    0x04
-#define LCD_MOVELEFT    0x00
+#define LCD_MOVELEFT     0x00
 
 // flags for function set
-#define LCD_8BITMODE    0x10
-#define LCD_4BITMODE    0x00
-#define LCD_2LINE    0x08
-#define LCD_5x8DOTS    0x00
+#define LCD_8BITMODE  0x10
+#define LCD_4BITMODE  0x00
+#define LCD_2LINE     0x08
+#define LCD_5x8DOTS   0x00
 
 //////////////////////////////////////////////////////////
 uint8_t _displayfunction = 0;
@@ -150,12 +150,18 @@ void turnOffRGB(I2CCONTEXT *rgb) {
 
 void initLCD(I2CCONTEXT *lcd) {
     //set the number of lines of our Display
-    writeByteRegister(lcd->file, 0x00, LCD_FUNCTIONSET | LCD_2LINE );
+    writeByteRegister(lcd->file, 0x00,
+		    LCD_FUNCTIONSET |
+		    LCD_2LINE |
+		    LCD_8BITMODE );
     usleep(100);
 
     //enable display, show cursor, show a blinking box cursor
-    writeByteRegister(lcd->file, 0x00,LCD_DISPLAYCONTROL | LCD_DISPLAYON 
-| LCD_CURSORON | LCD_BLINKON );
+    writeByteRegister(lcd->file, 0x00,
+		    LCD_DISPLAYCONTROL |
+		    LCD_DISPLAYON |
+		    LCD_CURSORON |
+		    LCD_BLINKON );
     usleep(100);
 
     //clear display and set cursor at 0 position
@@ -165,9 +171,10 @@ void initLCD(I2CCONTEXT *lcd) {
     //set how the cursor will move everytime a letter
     //is written to the LCD
     
-    writeByteRegister(lcd->file, 0x00,LCD_ENTRYMODESET | LCD_ENTRYLEFT | 
-LCD_ENTRYSHIFTDECREMENT );
-
+    writeByteRegister(lcd->file, 0x00,
+		    LCD_ENTRYMODESET |
+		    LCD_ENTRYLEFT |
+		    LCD_ENTRYSHIFTDECREMENT );
 }
 
 void writeToLCD(I2CCONTEXT *lcd, char *c) {
