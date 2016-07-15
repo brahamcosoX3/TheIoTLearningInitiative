d5565609efe4f8da8d813013ee46d53fdb81b0bd
Carlos Rodriguez <brahamcoso@gmail.com>
2016-07-15 21:18:47 +0000

    Adding the functions to write to the LCD
    This change adds the functionality to configure
    and write to the LCD
    
    Signed-off-by: Carlos Rodriguez <brahamcoso@gmail.com>

--- Sensors/I2CDL/lcd.c
+++ Sensors/I2CDL/lcd.c
@@ -9,6 +9,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #define RGB_SLAVE       0x62
 #define LCD_SLAVE       0x3E
@@ -19,6 +20,48 @@
 #define REG_MODE1       0x00
 #define REG_MODE2       0x01
 #define REG_OUTPUT      0x08
+
+// commands
+#define LCD_CLEARDISPLAY    0x01
+#define LCD_RETURNHOME      0x02
+#define LCD_ENTRYMODESET    0x04
+#define LCD_DISPLAYCONTROL    0x08
+#define LCD_CURSORSHIFT    0x10
+#define LCD_FUNCTIONSET 0x20
+#define LCD_SETCGRAMADDR    0x40
+#define LCD_SETDDRAMADDR    0x80
+
+// flags for display entry mode
+#define LCD_ENTRYRIGHT    0x00
+#define LCD_ENTRYLEFT    0x02
+#define LCD_ENTRYSHIFTINCREMENT 0x01
+#define LCD_ENTRYSHIFTDECREMENT 0x00
+
+// flags for display on/off control
+#define LCD_DISPLAYON    0x04
+#define LCD_DISPLAYOFF    0x00
+#define LCD_CURSORON    0x02
+#define LCD_CURSOROFF    0x00
+#define LCD_BLINKON    0x01
+#define LCD_BLINKOFF    0x00
+
+// flags for display/cursor shift
+#define LCD_DISPLAYMOVE 0x08
+#define LCD_CURSORMOVE    0x00
+#define LCD_MOVERIGHT    0x04
+#define LCD_MOVELEFT    0x00
+
+// flags for function set
+#define LCD_8BITMODE    0x10
+#define LCD_4BITMODE    0x00
+#define LCD_2LINE    0x08
+#define LCD_5x8DOTS    0x00
+
+//////////////////////////////////////////////////////////
+uint8_t _displayfunction = 0;
+uint8_t _displaycontrol = 0;
+uint8_t _displaymode = 0;
+uint8_t lines = 2;
 
 typedef struct
 {
@@ -31,54 +74,49 @@ typedef struct
 I2CCONTEXT lcd; //will store the lcd controller context
 I2CCONTEXT rgb; //will store the rgb controller context
 
-int initContext(I2CCONTEXT *ctx, int addr_,int bus)
-{
+int initContext(I2CCONTEXT *ctx, int addr_,int bus) {
     ctx->addr = addr_;
     ctx->adapter_nr = bus;
     snprintf(ctx->filename, 19, "/dev/i2c-%d", ctx->adapter_nr);
     ctx->file = open(ctx->filename, O_RDWR);
 
-    if (ctx->file < 0) 
-    {
+    if (ctx->file < 0) {
        /* ERROR HANDLING; you can check errno 2 c what went wrong */
         printf("Error ocurred @ opening BUS: (errno:%d) %s\n",
                 errno,
                 strerror(errno));
-        return -errno;    
 
+        return -errno;    
     }
 
-    if (ioctl(ctx->file, I2C_SLAVE, ctx->addr) < 0)
-    {
+    if (ioctl(ctx->file, I2C_SLAVE, ctx->addr) < 0) {
         /* ERROR HANDLING; you can check errno 2 c what went wrong */
         printf("Error ocurred @ accessing slave: (errno:%d) %s\n",
                     errno,
                     strerror(errno));
+
         return -errno;
     }
-
 }
 
-__s32 writeByteRegister(int file, __u8 register_, __u8 value)
-{
+__s32 writeByteRegister(int file, __u8 register_, __u8 value) {
     __s32 res = -1;
+
     res = i2c_smbus_write_byte_data(file, register_, value);
-    if (res < 0)
-    {
+    if (res < 0) {
         /* ERROR HANDLING: i2c transaction failed */
         printf("Error writing byte, (errno:%d),%s",errno,
                 strerror(errno));
     }
 }
 
-__s32 readRegister(int register_, int file)
-{
+__s32 readRegister(int register_, int file) {
     __u8 reg = register_;
     __s32 res = -1;
-    char buf[10];  
+    char buf[10];
+
     res = i2c_smbus_read_word_data(file, reg);
-    if (res < 0)
-    {
+    if (res < 0) {
         /* ERROR HANDLING: i2c transaction failed */
         printf("Error reading reg: 0x%x, (errno:%d),%s",
                 reg,errno,strerror(errno));
@@ -86,46 +124,98 @@ __s32 readRegister(int register_, int file)
     return res;
 }
 
-void setRGBColor(I2CCONTEXT *rgb, int r, int g, int b)
-{
+void setRGBColor(I2CCONTEXT *rgb, int r, int g, int b) {
     writeByteRegister(rgb->file, REG_RED, r);
     writeByteRegister(rgb->file, REG_GREEN, g);
     writeByteRegister(rgb->file, REG_BLUE, b);            
 }
 
-void initRGB(I2CCONTEXT *rgb)
-{
+void initRGB(I2CCONTEXT *rgb) {
     // backlight init
     writeByteRegister(rgb->file, REG_MODE1, 0);
+
     // set LEDs controllable by both PWM and GRPPWM registers
     writeByteRegister(rgb->file, REG_OUTPUT, 0xFF);
+
     // set MODE2 values
     writeByteRegister(rgb->file, REG_MODE2, 0x20);
+
     // set the baklight Color to white :)
     setRGBColor(rgb, 0xFF, 0xFF, 0xFF);    
 }
 
-void turnOffRGB(I2CCONTEXT *rgb)
-{
+void turnOffRGB(I2CCONTEXT *rgb) {
     setRGBColor(rgb, 0x00, 0x00, 0x00);    
 }
 
-int main()
-{
+void initLCD(I2CCONTEXT *lcd) {
+    //set the number of lines of our Display
+    writeByteRegister(lcd->file, 0x00, LCD_FUNCTIONSET | LCD_2LINE );
+    usleep(100);
+
+    //enable display, show cursor, show a blinking box cursor
+    writeByteRegister(lcd->file, 0x00,LCD_DISPLAYCONTROL | LCD_DISPLAYON 
+| LCD_CURSORON | LCD_BLINKON );
+    usleep(100);
+
+    //clear display and set cursor at 0 position
+    writeByteRegister(lcd->file, 0x00, LCD_CLEARDISPLAY );
+    usleep(4000);
+
+    //set how the cursor will move everytime a letter
+    //is written to the LCD
+    
+    writeByteRegister(lcd->file, 0x00,LCD_ENTRYMODESET | LCD_ENTRYLEFT | 
+LCD_ENTRYSHIFTDECREMENT );
+
+}
+
+void writeToLCD(I2CCONTEXT *lcd, char *c) {
+    uint ascii_val=0;
+    int i;
+    int s = strlen(c);
+
+    for(i = 0; i < s; i++) {
+        ascii_val = toascii(c[i]);
+        ///////////////////////////////////////////////////////////
+        //sending a 0x40 tell the LCD we will send a character code
+        //to CGRAM
+        ///////////////////////////////////////////////////////////
+        writeByteRegister(lcd->file, 0x40, ascii_val);
+    }
+}
+
+void turnOffLCD(I2CCONTEXT *lcd) {    
+    writeByteRegister(lcd->file, 0x00, LCD_DISPLAYCONTROL );
+    writeByteRegister(lcd->file, 0x00, LCD_CLEARDISPLAY );
+}
+
+
+int main() {
     /*
     we pass a reference to the rgb context variable
     the i2c address of the rgb controller
     and the BUS which should be 1
     */
 
+    //Grove LCD has two devices one that drives LCD
+    //and a second that drives RGB
+
     initContext(&rgb, RGB_SLAVE , BUS);
+    initContext(&lcd, LCD_SLAVE , BUS);
 
     /*turn on RGB LEDS*/
+    initLCD(&lcd);
     initRGB(&rgb);
+
+    writeToLCD(&lcd, ":) HOLO!");
+
     /*sleep for 5 secs before turning off*/
     sleep(5);
+
     /*turn off RGB LEDS*/
     turnOffRGB(&rgb);
+    turnOffLCD(&lcd);
 
     printf("\nDONE!\n");
 
